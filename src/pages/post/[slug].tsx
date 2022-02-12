545345ac9e3783da92efe087ec539ceade482a5f
Everton Xavier <evertonfxavier@gmail.com>
2022-02-12 14:02:37 -0300

    update

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,14 +1,7 @@
-import {
-  ReactNode,
-  Fragment,
-  Key,
-  ReactChild,
-  ReactFragment,
-  ReactPortal,
-} from "react";
+import { ReactNode, Fragment } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { GetServerSideProps, GetStaticPaths, GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Prismic from "@prismicio/client";
 import { RichText } from "prismic-dom";
 import { format, parseISO } from "date-fns";
@@ -32,17 +25,35 @@ interface Post {
       url: string;
     };
     author: string;
-    contents: Array<any>;
+    content: {
+      heading: string;
+      body: {
+        text: string;
+      }[];
+    }[];
   };
 }
 
 interface PostProps {
   post: Post;
-  // contents: any;
 }
 
-export default function Post({ post }: PostProps) {
+export default function Post({ post }: PostProps): ReactNode {
   const { isFallback } = useRouter();
+  const datePost = format(parseISO(post.first_publication_date), "d MMM yyy", {
+    locale: ptBR,
+  });
+
+  const formattedPost = {
+    ...post,
+    data: {
+      ...post.data,
+      content: post.data.content.map((c) => ({
+        ...c,
+        body: RichText.asHtml(c.body),
+      })),
+    },
+  };
 
   return (
     <>
@@ -61,7 +72,7 @@ export default function Post({ post }: PostProps) {
               <article className={styles.PostTitle}>
                 <div>
                   <FiCalendar />
-                  <time>{post.first_publication_date}</time>
+                  <time>{datePost}</time>
                 </div>
                 <div>
                   <FiUser />
@@ -73,8 +84,8 @@ export default function Post({ post }: PostProps) {
                 </div>
               </article>
               <section>
-                {post.data.contents.map((c: any, idx: Key) => (
-                  <Fragment key={idx}>
+                {formattedPost.data.content.map((c) => (
+                  <Fragment key={c.heading}>
                     <h2>{c.heading}</h2>
                     <div dangerouslySetInnerHTML={{ __html: c.body }} />
                   </Fragment>
@@ -91,7 +102,7 @@ export default function Post({ post }: PostProps) {
 export const getStaticPaths: GetStaticPaths = async () => {
   const prismic = getPrismicClient();
   const posts = await prismic.query([
-    Prismic.predicates.at("document.type", "post"),
+    Prismic.predicates.at("document.type", "posts"),
   ]);
 
   const paths = posts.results.map((post) => ({
@@ -104,38 +115,34 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { slug } = params;
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { slug } = context.params;
 
   const prismic = getPrismicClient();
   const response = await prismic.getByUID("posts", String(slug), {});
 
   const post = {
     uid: response.uid,
-    first_publication_date: format(
-      parseISO(response.first_publication_date),
-      "d MMM yyy",
-      { locale: ptBR }
-    ),
+    first_publication_date: response.first_publication_date,
     data: {
-      title: RichText.asText(response.data.title),
-      author: RichText.asText(response.data.author),
-      subtitle: RichText.asText(response.data.subtitle),
+      title: response.data.title,
+      author: response.data.author,
+      subtitle: response.data.subtitle,
       banner: {
         url: response.data.banner.url,
       },
-      contents: response.data.content.map(
-        (item: { heading: string; body: string }) => {
-          return {
-            heading: RichText.asText(item.heading),
-            body: RichText.asHtml(item.body),
-          };
-        }
-      ),
+      content: response.data.content.map((c) => ({
+        heading: c.heading,
+        body: c.body.map((b) => ({
+          ...b,
+        })),
+      })),
     },
   };
 
   return {
-    props: { post },
+    props: {
+      post,
+    },
   };
 };
